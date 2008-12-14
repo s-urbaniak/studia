077545836bfdf6bd6b6b6df292509b48c78302c5
sergiusz.urbaniak <sergiusz.urbaniak@518361b8-c369-11dd-aab6-efc349ba664b>
2008-12-14 11:13:35 +0000

    labs

--- rok1/kodowanie/hamming.m
+++ rok1/kodowanie/hamming.m
@@ -1,66 +1,91 @@
 expo = 2;
 
-% generate random binary vector
-in_len = 2^expo;
-in = rand(1, in_len);
-in = in.*2;
-in = floor(in);
+function out = generate_random_vector(expo)
+    expo
+    % generate random binary vector
+    out_len = 2^expo;
 
-% calculate amount of parity bits
-r = expo+1;
+    % set random values in vector
+    out = rand(1, out_len);
+    out = out .* 2;
+    out = floor(out);
+endfunction
 
 % calculate parity bit positions
-r_pos = 1:r;
-for i = r_pos
-    r_pos(i) = 2^(i-1);
-endfor
+function parity_pos = parity_bit_positions (expo)
+    r = expo+1;
 
-in
+    parity_pos = 1:r;
+    for i = parity_pos
+        parity_pos(i) = 2^(i-1);
+    endfor
+endfunction
 
 % insert zeros at parity bit positions
-in_len = numel(in)+numel(r_pos);
-tf=false(1,in_len);
-in_new=double(tf);
-tf(r_pos)=true;
-in_new(tf)=zeros(1,r) .+ 0;
-in_new(~tf)=in;
-in = in_new;
-in
+function out = insert_empty_parities (in, expo)
+    r = expo+1;
+    r_pos = parity_bit_positions(expo);
 
-% construct parity matrix
-parity_matrix = zeros(r, in_len);
-for i = 1:in_len
-    x = i;
-    y = [];
-    % Enter the main loop,
-    while x > 1
-        y = [y mod(x,2)]; % concatenating each binary digit,
-        x = floor(x/2);   % updating the quotient.
-    end
-    y = [y x];            % Concatenate the last quotient.
-    y = [y zeros(1, r-numel(y))];
+    in_len = numel(in)+r;
+    tf=false(1,in_len);
+    in_new=double(tf);
+    tf(r_pos)=true;
+    in_new(tf)=zeros(1,r) .+ 0;
+    in_new(~tf)=in;
+    out = in_new;
+endfunction
 
-    y = transpose(y);
-    parity_matrix(:,i) = y;
-endfor
-parity_matrix
+function parity_matrix = build_parity_matrix (expo)
+    r = expo+1;
+    in_len = 2^expo + r;
 
-% calculate parities
-for i = 1:r
-    row = parity_matrix(i,:);
-    paritiy_vals = and(row,in);
+    for i = 1:in_len
+        x = i;
+        y = [];
+        % Enter the main loop,
+        while x > 1
+            y = [y mod(x,2)]; % concatenating each binary digit,
+            x = floor(x/2);   % updating the quotient.
+        end
+        y = [y x];            % Concatenate the last quotient.
+        y = [y zeros(1, r-numel(y))];
 
-    p_old = 0;
-    for p = paritiy_vals
-        p_old = xor(p, p_old);
+        y = transpose(y);
+        parity_matrix(:,i) = y;
     endfor
+endfunction
 
-    in(r_pos(i)) = p_old;
-endfor
-in
+function out = calculate_parities (in, expo)
+    r = expo+1;
+    parity_matrix = build_parity_matrix(expo);
+    out = in;
+    r_pos = parity_bit_positions(expo);
+
+    % calculate parities
+    for i = 1:r
+        % parity bits at index i = Li
+        parities = parity_matrix(i,:);
+        paritiy_vals = and(parities, in);
+        % reset parity bits of falsified vector
+        paritiy_vals(r_pos(i)) = 0;
+
+        p_old = 0;
+        for p = paritiy_vals
+            p_old = xor(p, p_old);
+        endfor
+
+        out(r_pos(i)) = p_old;
+    endfor
+endfunction
+
+in = generate_random_vector (expo)
+in = insert_empty_parities(in, expo);
+in = calculate_parities(in, expo)
+parity_matrix = build_parity_matrix(expo)
 
 % falsify one bit
-false_index = 3
+false_index = 5
 in_false = in;
 in_false(false_index) = not(in(false_index))
+out = calculate_parities(in_false, expo)
 
